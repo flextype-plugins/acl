d46ae9bf0cc66c253d42f6079b168094e5c3e84d
Awilum <awilum@msn.com>
2020-12-20 13:09:45 +0300

    ACL 1.6.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1 align="center">ACL Plugin for <a href="http://flextype.org/">Flextype</a></h1>
 
 <p align="center">
-<a href="https://github.com/flextype-plugins/acl/releases"><img alt="Version" src="https://img.shields.io/github/release/flextype-plugins/acl.svg?label=version&color=black"></a> <a href="https://github.com/flextype-plugins/acl"><img src="https://img.shields.io/badge/license-MIT-blue.svg?color=black" alt="License"></a> <a href="https://github.com/flextype-plugins/acl"><img src="https://img.shields.io/github/downloads/flextype-plugins/acl/total.svg?color=black" alt="Total downloads"></a> <a href="https://github.com/flextype/flextype"><img src="https://img.shields.io/badge/Flextype-0.9.11-green.svg" alt="Flextype"></a> <a href=""><img src="https://img.shields.io/discord/423097982498635778.svg?logo=discord&color=black&label=Discord%20Chat" alt="Discord"></a>
+<a href="https://github.com/flextype-plugins/acl/releases"><img alt="Version" src="https://img.shields.io/github/release/flextype-plugins/acl.svg?label=version&color=black"></a> <a href="https://github.com/flextype-plugins/acl"><img src="https://img.shields.io/badge/license-MIT-blue.svg?color=black" alt="License"></a> <a href="https://github.com/flextype-plugins/acl"><img src="https://img.shields.io/github/downloads/flextype-plugins/acl/total.svg?color=black" alt="Total downloads"></a> <a href="https://github.com/flextype/flextype"><img src="https://img.shields.io/badge/Flextype-0.9.13-green.svg" alt="Flextype"></a> <a href=""><img src="https://img.shields.io/discord/423097982498635778.svg?logo=discord&color=black&label=Discord%20Chat" alt="Discord"></a>
 </p>
 
 ## Features
@@ -15,7 +15,7 @@ The following dependencies need to be downloaded and installed for ACL Plugin.
 
 | Item | Version | Download |
 |---|---|---|
-| [flextype](https://github.com/flextype/flextype) | 0.9.12 | [download](https://github.com/flextype/flextype/releases) |
+| [flextype](https://github.com/flextype/flextype) | 0.9.13 | [download](https://github.com/flextype/flextype/releases) |
 | [twig](https://github.com/flextype-plugins/twig) | >=1.0.0 | [download](https://github.com/flextype-plugins/twig/releases) |
 
 ## Installation
@@ -564,4 +564,4 @@ echo 'and your roles: ' . flextype('acl')->getUserLoggedInRoles();
 
 ## LICENSE
 [The MIT License (MIT)](https://github.com/flextype-plugins/acl/blob/master/LICENSE.txt)
-Copyright (c) 2020 [Sergey Romanenko](https://github.com/Awilum)
+Copyright (c) 2021 [Sergey Romanenko](https://github.com/Awilum)
--- shortcodes/AclShortcodesExtension.php
+++ shortcodes/AclShortcodesExtension.php
@@ -12,7 +12,7 @@ namespace Flextype\Plugin\Acl\Shortcodes;
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 // Shortcode: [userLoggedInEmail]
-flextype('shortcode')->addHandler('userLoggedInEmail', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInEmail', static function (ShortcodeInterface $s) {
     if (flextype('acl')->getUserLoggedInEmail()) {
         return flextype('acl')->getUserLoggedInEmail();
     }
@@ -21,7 +21,7 @@ flextype('shortcode')->addHandler('userLoggedInEmail', static function (Shortcod
 });
 
 // Shortcode: [userLoggedInUuid]
-flextype('shortcode')->addHandler('userLoggedInUuid', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInUuid', static function (ShortcodeInterface $s) {
     if (flextype('acl')->getUserLoggedInUuid()) {
         return flextype('acl')->getUserLoggedInUuid();
     }
@@ -30,7 +30,7 @@ flextype('shortcode')->addHandler('userLoggedInUuid', static function (Shortcode
 });
 
 // Shortcode: [userLoggedInRoles]
-flextype('shortcode')->addHandler('userLoggedInRoles', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInRoles', static function (ShortcodeInterface $s) {
     if (flextype('acl')->getUserLoggedInRoles()) {
         return flextype('acl')->getUserLoggedInRoles();
     }
@@ -39,7 +39,7 @@ flextype('shortcode')->addHandler('userLoggedInRoles', static function (Shortcod
 });
 
 // Shortcode: [userLoggedIn]Private content here..[/userLoggedIn]
-flextype('shortcode')->addHandler('userLoggedIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedIn', static function (ShortcodeInterface $s) {
     if (flextype('acl')->isUserLoggedIn()) {
         return $s->getContent();
     }
@@ -48,7 +48,7 @@ flextype('shortcode')->addHandler('userLoggedIn', static function (ShortcodeInte
 });
 
 // Shortcode: [userLoggedInRolesIn roles="admin, student"]Private content here..[/userLoggedInRolesIn]
-flextype('shortcode')->addHandler('userLoggedInRolesIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInRolesIn', static function (ShortcodeInterface $s) {
     if (flextype('acl')->isUserLoggedInRolesIn($s->getParameter('roles'))) {
         return $s->getContent();
     }
@@ -57,7 +57,7 @@ flextype('shortcode')->addHandler('userLoggedInRolesIn', static function (Shortc
 });
 
 // Shortcode: [userLoggedInUuidIn uuids="ea7432a3-b2d5-4b04-b31d-1c5acc7a55e2, d549af27-79a0-44f2-b9b1-e82b47bf87e2"]Private content here..[/userLoggedInUuidOneOf]
-flextype('shortcode')->addHandler('userLoggedInUuidIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInUuidIn', static function (ShortcodeInterface $s) {
     if (flextype('acl')->isUserLoggedInUuidIn($s->getParameter('uuids'))) {
         return $s->getContent();
     }
@@ -66,7 +66,7 @@ flextype('shortcode')->addHandler('userLoggedInUuidIn', static function (Shortco
 });
 
 // Shortcode: [userLoggedInEmailIn emails="jack@flexype.org, sam@flextype.org"]Private content here..[/userLoggedInUsernameOneOf]
-flextype('shortcode')->addHandler('userLoggedInEmailIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInEmailIn', static function (ShortcodeInterface $s) {
     if (flextype('acl')->isUserLoggedInEmailIn($s->getParameter('emails'))) {
         return $s->getContent();
     }
@@ -75,7 +75,7 @@ flextype('shortcode')->addHandler('userLoggedInEmailIn', static function (Shortc
 });
 
 // Shortcode: [userNotLoggedIn]Content for not logged in user here..[/userNotLoggedIn]
-flextype('shortcode')->addHandler('userNotLoggedIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userNotLoggedIn', static function (ShortcodeInterface $s) {
     if (! flextype('acl')->isUserLoggedIn()) {
         return $s->getContent();
     }
@@ -84,7 +84,7 @@ flextype('shortcode')->addHandler('userNotLoggedIn', static function (ShortcodeI
 });
 
 // Shortcode: [userLoggedInRolesNotIn roles="admin, student"]Private content here..[/userLoggedInRolesNotIn]
-flextype('shortcode')->addHandler('userLoggedInRolesNotIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInRolesNotIn', static function (ShortcodeInterface $s) {
     if (! flextype('acl')->isUserLoggedInRolesNotIn($s->getParameter('roles'))) {
         return $s->getContent();
     }
@@ -93,7 +93,7 @@ flextype('shortcode')->addHandler('userLoggedInRolesNotIn', static function (Sho
 });
 
 // Shortcode: [userLoggedInUuidNotIn uuids="ea7432a3-b2d5-4b04-b31d-1c5acc7a55e2, d549af27-79a0-44f2-b9b1-e82b47bf87e2"]Private content here..[/userLoggedInUuidNotIn]
-flextype('shortcode')->addHandler('userLoggedInUuidNotIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInUuidNotIn', static function (ShortcodeInterface $s) {
     if (! flextype('acl')->isUserLoggedInUuidNotIn($s->getParameter('uuids'))) {
         return $s->getContent();
     }
@@ -102,7 +102,7 @@ flextype('shortcode')->addHandler('userLoggedInUuidNotIn', static function (Shor
 });
 
 // Shortcode: [userLoggedInEmailNotIn emails="jack@flexype.org, sam@flextype.org"]Private content here..[/userLoggedInEmailNotIn]
-flextype('shortcode')->addHandler('userLoggedInEmailNotIn', static function (ShortcodeInterface $s) {
+flextype('parsers')->shortcode()->addHandler('userLoggedInEmailNotIn', static function (ShortcodeInterface $s) {
     if (! flextype('acl')->isUserLoggedInEmailsNotIn($s->getParameter('emails'))) {
         return $s->getContent();
     }
